9cedfc989856716b30204437d7cdba8016977746
BobSaidHi <47067448+BobSaidHi@users.noreply.github.com>
2026-03-27 03:00:35 -0700

    Create FSMStates.hpp

---
+++ 2026Core/FSMStates.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <cstdint>
+
+namespace FSMCommon {
+    enum States : uint_fast8_t {
+        sINIT = 0,
+        sESTOP,
+        sRST,
+        sStartLoad,
+        sSRunLoad,
+        sCurtail
+    };
+}
