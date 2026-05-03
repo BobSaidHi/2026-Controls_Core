2a342c1cb2975ad8a2812293696aaa3bec6247ea
BobSaidHi <47067448+BobSaidHi@users.noreply.github.com>
2026-05-02 21:00:25 -0700

    Formatted TurbinePacket

--- 2026Core/TurbinePacket/TurbinePacket.cpp
+++ 2026Core/TurbinePacket/TurbinePacket.cpp
@@ -4,17 +4,19 @@
 // Create packets
 // =====================
 
-void makeNacellePacket(NacellePacket &packet, int16_t rpm, int16_t angularAccell_RPMPS) {
-  packet.rpm = rpm;
-  packet.angularAccell_RPMPS = angularAccell_RPMPS;
+void makeNacellePacket(NacellePacket &packet, int16_t rpm,
+                       int16_t angularAccell_RPMPS) {
+    packet.rpm = rpm;
+    packet.angularAccell_RPMPS = angularAccell_RPMPS;
 }
 
 void makeLoadboxPacket(LoadboxPacket &packet, int16_t d_mVPS,
-                       int16_t current_mA, int16_t dIPS, ESTOP_TYPE_NET safety) {
-  packet.d_mVPS = d_mVPS;
-  packet.current_mA = current_mA;
-  packet.dIPS = dIPS;
-  packet.safety = safety;
+                       int16_t current_mA, int16_t dIPS,
+                       ESTOP_TYPE_NET safety) {
+    packet.d_mVPS = d_mVPS;
+    packet.current_mA = current_mA;
+    packet.dIPS = dIPS;
+    packet.safety = safety;
 }
 
 // =====================
@@ -22,24 +24,24 @@ void makeLoadboxPacket(LoadboxPacket &packet, int16_t d_mVPS,
 // =====================
 
 void printNacellePacket(const NacellePacket &packet, Stream &out) {
-  out.println("---- NacellePacket ----");
-  out.print("RPM: ");
-  out.println(packet.rpm);
-  out.print("dRPM/s: ");
-  out.println(packet.angularAccell_RPMPS);
-  out.println("-----------------------");
+    out.println("---- NacellePacket ----");
+    out.print("RPM: ");
+    out.println(packet.rpm);
+    out.print("dRPM/s: ");
+    out.println(packet.angularAccell_RPMPS);
+    out.println("-----------------------");
 }
 
 void printLoadboxPacket(const LoadboxPacket &packet, Stream &out) {
-  out.println("---- LoadboxPacket ----");
-  out.print("dV/s: ");
-  out.println(packet.d_mVPS);
-  out.print("Current: ");
-  out.println(packet.current_mA);
-  out.print("dC/s: ");
-  out.println(packet.dIPS);
-  out.print("Safety: ");
-  out.println(static_cast<uint_fast8_t>(packet.safety));
+    out.println("---- LoadboxPacket ----");
+    out.print("dV/s: ");
+    out.println(packet.d_mVPS);
+    out.print("Current: ");
+    out.println(packet.current_mA);
+    out.print("dC/s: ");
+    out.println(packet.dIPS);
+    out.print("Safety: ");
+    out.println(static_cast<uint_fast8_t>(packet.safety));
 
-  out.println("-----------------------");
+    out.println("-----------------------");
 }
--- 2026Core/TurbinePacket/TurbinePacket.hpp
+++ 2026Core/TurbinePacket/TurbinePacket.hpp
@@ -9,25 +9,26 @@ const uint8_t SENDER_LOADBOX = 2;
 
 // Estop types
 enum class ESTOP_TYPE_NET : uint8_t {
-  NONE = 0,
-  BUTTON = 1,
-  LOAD_DISCONNECT = 2,
-  RESERVED = static_cast<uint8_t>(-1)
+    NONE = 0,
+    BUTTON = 1,
+    LOAD_DISCONNECT = 2,
+    RESERVED = static_cast<uint8_t>(-1)
 };
 
 enum class ESTOP_TYPE_FAST : uint_fast8_t {
-  NONE = static_cast<uint_fast8_t>(ESTOP_TYPE_NET::NONE),
-  BUTTON = static_cast<uint_fast8_t>(ESTOP_TYPE_NET::BUTTON),
-  LOAD_DISCONNECT = static_cast<uint_fast8_t>(ESTOP_TYPE_NET::LOAD_DISCONNECT),
-  RESERVED = static_cast<uint_fast8_t>(ESTOP_TYPE_NET::RESERVED)
+    NONE = static_cast<uint_fast8_t>(ESTOP_TYPE_NET::NONE),
+    BUTTON = static_cast<uint_fast8_t>(ESTOP_TYPE_NET::BUTTON),
+    LOAD_DISCONNECT =
+        static_cast<uint_fast8_t>(ESTOP_TYPE_NET::LOAD_DISCONNECT),
+    RESERVED = static_cast<uint_fast8_t>(ESTOP_TYPE_NET::RESERVED)
 };
 
 // =====================
 // NACELLE → LOADBOX
 // =====================
 struct NacellePacket {
-  int16_t rpm;
-  int16_t angularAccell_RPMPS;
+    int16_t rpm;
+    int16_t angularAccell_RPMPS;
 };
 
 // =====================
@@ -46,20 +47,12 @@ struct LoadboxPacket {
 // =====================
 
 // Build packets
-void makeNacellePacket(
- NacellePacket &packet,
- int16_t rpm,
- int16_t angularAccell_RPMPS
- );
+void makeNacellePacket(NacellePacket &packet, int16_t rpm,
+                       int16_t angularAccell_RPMPS);
 
 // DONE: add power stuff
-void makeLoadboxPacket(
-  LoadboxPacket &packet,
-  int16_t d_mVPS,
-  int16_t current_mA,
-  int16_t dIPS,
-  ESTOP_TYPE_NET safety
-);
+void makeLoadboxPacket(LoadboxPacket &packet, int16_t d_mVPS,
+                       int16_t current_mA, int16_t dIPS, ESTOP_TYPE_NET safety);
 
 // Debug printing
 void printNacellePacket(const NacellePacket &packet, Stream &out);
