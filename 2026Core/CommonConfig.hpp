e35db8ed25007f619f100d42346fc0d230f0a314
BobSaidHi <47067448+BobSaidHi@users.noreply.github.com>
2026-05-06 17:59:02 -0700

    Moved all unit conversion constants to CommonConfig

--- 2026Core/CommonConfig.hpp
+++ 2026Core/CommonConfig.hpp
@@ -44,28 +44,43 @@ namespace WTbCommonConfig {
     // constexpr uint_fast8_t DEBUG_LEVEL = CT_LOG_LEVEL_MACRO; // todo
     // CONFIG - Size of string to store debug message
     constexpr uint_fast8_t MAX_MSG_SIZE = 40; // todo
-    constexpr unsigned long SERIAL_BAUD = 115200; // CONFIG - Baud rate for serial communication
+    constexpr unsigned long SERIAL_BAUD =
+        115200; // CONFIG - Baud rate for serial communication
 
-    /* Additional Checks*/
-    constexpr uint_fast8_t TIME_TO_MICROS_ROLLOVER_MINS = UINT32_MAX / 1000000 / 60;
-    constexpr uint_fast8_t TIME_TO_MILLIS_ROLLOVER_DAYS =
-        UINT32_MAX / 1000 / 60 / 60 / 24;
-
-} // end namespace WTbCommonConfig
-
-// MARK: Constants
-namespace CONSTS {
     /* Additional Checks*/
     constexpr uint_fast8_t TIME_TO_MICROS_ROLLOVER_MINS =
         UINT32_MAX / 1000000 / 60;
     constexpr uint_fast8_t TIME_TO_MILLIS_ROLLOVER_DAYS =
         UINT32_MAX / 1000 / 60 / 60 / 24;
 
-    /* Time Constants */
-    // constexpr uint32_t MILLIS_PER_SEC = 1000;
-    // constexpr uint32_t SECS_PER_MIN = 60;
+} // end namespace WTbCommonConfig
 
-} // end namespace CONSTS
+// MARK: Constants
+namespace UNITS {
+    /* Additional Checks*/
+    constexpr uint_fast8_t TIME_TO_MICROS_ROLLOVER_MINS =
+        UINT32_MAX / 1000000 / 60;
+    constexpr uint_fast8_t TIME_TO_MILLIS_ROLLOVER_DAYS =
+        UINT32_MAX / 1000 / 60 / 60 / 24;
+
+    /* General Conversions */
+    constexpr unsigned long m_TO_BASE = 1000;
+    constexpr unsigned long u_TO_m = 1000;
+    constexpr unsigned long u_TO_BASE = m_TO_BASE * u_TO_m;
+
+    /* Time Constants */
+    constexpr uint32_t SECS_PER_MIN = 60;
+    constexpr uint32_t MILLIS_PER_SEC = m_TO_BASE;
+    constexpr uint32_t MICROS_PER_MILLI = u_TO_m;
+    constexpr uint32_t MICROS_PER_SEC = u_TO_BASE;
+
+    /* Voltage Constants */
+    constexpr uint_fast16_t mV_PER_V = m_TO_BASE;
+    constexpr uint_fast16_t uV_PER_mV = u_TO_m;
+    constexpr uint_fast16_t uV_PER_V = mV_PER_V * uV_PER_mV;
+    constexpr uint_fast16_t uVOLTS_PER_VOLT = uV_PER_V;
+
+} // end namespace UNITS
 
 /**
  * TODO: Detect I2C discount, safely exit, and reconnect
